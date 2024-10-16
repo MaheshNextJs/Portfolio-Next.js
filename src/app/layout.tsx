651d8964132c88b3fddcd9f74e65a0cb2f62d035
MaheshNextJs <maheshbairi3@gmail.com>
2024-10-16 10:48:23 +0530

    name changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Mahesh Bairi Portfolio",
-  description: "Mahesh Bairi",
+  description: "Mahesh Bairi.",
 };
 
 export default function RootLayout({
