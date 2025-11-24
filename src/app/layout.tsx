aa8d58b7825888fd97d6adbea2cc4df43a8a6cbe
MaheshNextJs <maheshbairi3@gmail.com>
2025-11-24 08:10:05 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,41 +1,22 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
-  title: "Mahesh Bairi Portfolio",
-  description: "Mahesh Bairi.",
+  title: "Mahesh Bairi | Frontend Developer",
+  description: "Portfolio of Mahesh Bairi - Frontend Web & Mobile Developer",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
